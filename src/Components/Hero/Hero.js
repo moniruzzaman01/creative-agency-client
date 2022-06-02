a9fb196edb0755f19e71958f14b9addc13719fe1
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-06-03 03:26:34 +0600

    retouch

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -3,9 +3,13 @@ import hero from "../../assets/logos/Frame.png";
 
 const Hero = () => {
   return (
-    <div className=" px-5 md:px-10 pb-20 h-full items-center min-h-[60vh] lg:px-32 lg:flex lg:flex-row-reverse ">
-      <img src={hero} className=" lg:max-w-xl mx-auto mb-10 lg:w-1/2" alt="" />
-      <div className=" max-w-3xl mx-auto lg:w-1/2 ">
+    <div className=" px-5 md:px-10 pb-20 h-full items-center min-h-[60vh] md:flex md:flex-row-reverse lg:px-32 lg:flex lg:flex-row-reverse ">
+      <img
+        src={hero}
+        className=" lg:max-w-xl mx-auto mb-10 md:w-1/2 lg:w-1/2"
+        alt=""
+      />
+      <div className=" max-w-3xl mx-auto md:w-1/2 lg:w-1/2 ">
         <h1 className="text-4xl lg:text-5xl font-bold">
           Let’s Grow Your Brand To The Next Level
         </h1>

ed88e0941997246af372f417739a4f06cba8bbbf
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-06-03 00:41:02 +0600

    home section retouch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./Components/Home/Home";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "../src/Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className=" max-w-7xl mx-auto">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,29 +1,29 @@
 import React from "react";
 import Hero from "../Hero/Hero";
-import Navbar from "../Navbar/Navbar";
 import Links from "./Links";
 
 const Home = () => {
   return (
-    <div>
+    <div className=" overflow-hidden">
       <div
-        className=" relative overflow-hidden "
+        className=" overflow-hidden "
         style={{
           backgroundColor: "#fbd062",
-          minHeight: "60vh",
+          minHeight: "55vh",
         }}
       >
-        <Navbar />
         <Hero />
-        {/* bg angler start */}
-        <div
-          style={{
-            width: "150%",
-          }}
-          className=" absolute top-[52vh] h-96 bg-white rotate-[-6deg]"
-        ></div>
-        {/* bg angler end */}
       </div>
+      {/* bg angler start */}
+      <div
+        style={{
+          width: "120%",
+          height: "15vh",
+          marginLeft: "-20px",
+        }}
+        className="bg-white rotate-[-6deg] mt-[-10vh]"
+      ></div>
+      {/* bg angler end */}
       <Links />
     </div>
   );

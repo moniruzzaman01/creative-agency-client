f9bfe5638115f6360ea7025bc63d8e7af0eb6233
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-06-04 01:59:11 +0600

    signup page created

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/logos/logo.png";
 import google from "../../assets/google.png";
 
-const Login = () => {
+const Signup = () => {
   const navigate = useNavigate();
 
   return (
@@ -16,16 +16,16 @@ const Login = () => {
       />
       <div className=" h-2/5 md:w-1/2 lg:h-1/2 border-2 rounded m-auto flex justify-center items-center ">
         <div className=" w-full p-10 ">
-          <h2 className=" text-3xl text-center font-bold mb-5">Login with</h2>
+          <h2 className=" text-3xl text-center font-bold mb-5">Sign Up</h2>
           <button className=" btn btn-outline hover:bg-transparent hover:text-[#DB4437] hover:border-[#DB4437] rounded-full w-full relative">
             <img className="absolute left-0 h-9 ml-1" src={google} alt="" />
-            Google sign in
+            Google signin
           </button>
           <div>
-            <p className=" text-sm text-center mt-2 disabled">
-              Don't have an account?{" "}
-              <Link className=" text-blue-600 line-through" to="#">
-                Create an account
+            <p className=" text-sm text-center mt-2">
+              have an account?{" "}
+              <Link className=" text-blue-600 underline" to="/login">
+                Login
               </Link>
             </p>
           </div>
@@ -35,4 +35,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;

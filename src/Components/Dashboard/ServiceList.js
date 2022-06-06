33dede4e38595f52dc42efb60cf5b88889a4c69e
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-06-07 00:48:33 +0600

    service added

--- src/Components/Dashboard/ServiceList.js
+++ src/Components/Dashboard/ServiceList.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 const ServiceList = () => {
-  return <div>Servicelist</div>;
+  return (
+    <div className=" overflow-x-scroll bg-white w-full rounded-xl p-3">
+      <div className=" min-w-[450px]">
+        <div className=" mb-2 bg-slate-200 w-full h-8 rounded-lg">
+          <div className=" grid grid-cols-5 text-center">
+            <p className=" text-xs leading-8 font-bold text-gray-500">Name</p>
+            <p className=" text-xs leading-8 font-bold text-gray-500">
+              Email ID
+            </p>
+            <p className=" text-xs leading-8 font-bold text-gray-500">
+              Service
+            </p>
+            <p className=" text-xs leading-8 font-bold text-gray-500">
+              Project Details
+            </p>
+            <p className=" text-xs leading-8 font-bold text-gray-500">Status</p>
+          </div>
+        </div>
+        <div className="  w-full h-8 rounded-lg">
+          <div className=" grid grid-cols-5 text-center">
+            <p className=" text-xs leading-8 text-gray-500">Moniruzzaman</p>
+            <p className=" text-xs leading-8 text-gray-500">Email ID</p>
+            <p className=" text-xs leading-8 text-gray-500">Service</p>
+            <p className=" text-xs leading-8 text-gray-500">Project Details</p>
+            <p className=" text-xs leading-8 text-gray-500">Status</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceList;
